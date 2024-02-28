48ccaf1e2a6bf240affbffb2bc636fc5f26c8510
NVadim <palaginvad@gmail.com>
2024-02-28 16:15:53 +0200

    mobile backBtn

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -76,7 +76,12 @@ function Main() {
                     </div>
                     <div className="mainContent__backBtn" onClick={toggleVisibility}>
                         <button >
+                            <span>
                             &lt; Stop Farm
+                            </span>
+                            <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 1.5L11 11.5M1 11.5L11 1.5" stroke="white" stroke-opacity="0.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
                         </button>
                     </div>
                     <div className="mainContent__coinBox">
