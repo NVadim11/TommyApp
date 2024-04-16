e0639fca99719c13c15487ef9b2587735989af5f
NVadim <palaginvad@gmail.com>
2024-04-16 13:52:40 +0300

    deploy test

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -381,7 +381,7 @@ function Main() {
 	 // Функция debounce для обработки клика
 	 const debouncedHandleClick = debounce(() => {
 		setCurrCoins((prevCoins) => prevCoins + clickNewCoins);
-	  }, 1000);  // Задержка в 500 мс
+	  }, 500);  // Задержка в 500 мс
 
 	const coinClicker = (event) => {
 		if (!event.isTrusted) return;
